da130716fb0673a05f113fa78f8c0e4c6e055fe2
Hypnos <1642232305@qq.com>
2020-12-19 23:06:15 +0800

    增加 登陆成功后生成个人信息cookie

--- src/main/java/com/demo/task/controller/LoginController.java
+++ src/main/java/com/demo/task/controller/LoginController.java
@@ -9,7 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.net.HttpCookie;
 
 @RestController
 public class LoginController {
@@ -19,31 +23,51 @@ public class LoginController {
     ClassService classService;
 
     @PostMapping("/login")
-    public ResponseEntity login(HttpServletRequest request){
+    public ResponseEntity login(HttpServletRequest request, HttpServletResponse response){
+
+        //将请求体中的参数提取，减少调用getparameter方法的次数
         String requestUserNum = request.getParameter("userNum");
         String requestPassWord = request.getParameter("passWord");
+        String requestIdentity = request.getParameter("Authentication");
 
+        //创建返回体与学生实体
         ResponseEntity loginResponse = new ResponseEntity();
         StudentEntity studentEntity = studentService.getStudent(requestUserNum);
 
+        //如果用户不存在
         if(studentEntity == null){
             loginResponse.setStatus(201);
             loginResponse.setMsg("用户不存在！");
         }
+        //如果用户密码与数据库不匹配
         else if(requestPassWord.equals(studentEntity.getPassWord())){
             loginResponse.setStatus(200);
             loginResponse.setMsg("成功！");
+
+            //创建userInfo，并添加数据，最后将userInfo放入响应体的data内
             UserInfo userInfo = new UserInfo();
             userInfo.setUserName(studentEntity.getName());
             userInfo.setUserNum(studentEntity.getNum());
             userInfo.setMajor(classService.getClass(studentEntity.getClassId()).getMajor());
             userInfo.setClassNum(classService.getClass(studentEntity.getClassId()).getNum());
             loginResponse.setData(userInfo);
+
+            //给响应消息添加cookie
+            response.addCookie(new Cookie("userNum",userInfo.getUserNum()));
+            response.addCookie(new Cookie("userName",userInfo.getUserName()));
+            response.addCookie(new Cookie("Major",userInfo.getMajor()));
+            response.addCookie(new Cookie("className",String.valueOf(userInfo.getClassNum())));
+
+//            HttpSession session = request.getSession();
+//            session.setAttribute("user",userInfo);
         }
+        //其他情况
         else{
             loginResponse.setStatus(201);
             loginResponse.setMsg("失败！");
         }
+
+        //返回响应体
         return loginResponse;
     }
 }
